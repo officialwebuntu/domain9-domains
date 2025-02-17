4dfd1619afd14faf47cbb2557b9c799d6041b150
electric-otter <electric-otter-github@proton.me>
2025-02-17 09:13:28 -0500

    Create domain.sql

---
+++ domain.sql
@@ -0,0 +1,8 @@
+CREATE TABLE domains (
+    id INTEGER PRIMARY KEY,
+    domain_name VARCHAR(255) NOT NULL,
+    registrant_name VARCHAR(255),
+    registrant_email VARCHAR(255),
+    dns_records TEXT,
+    expiration_date DATE
+);
